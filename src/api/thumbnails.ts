4d36f6f6d6a1c1807799d263706c69b2efe284dd
Kaden-Beck <kadenb12@gmail.com>
2026-05-04 22:22:04 -0600

    Added better uploads to instead utilize file storage

--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -4,39 +4,9 @@ import { getVideo, updateVideo } from "../db/videos";
 import type { ApiConfig } from "../config";
 import type { BunRequest } from "bun";
 import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
+import path from "node:path";
 
 const MAX_UPLOAD_SIZE = 10 << 20; // 10 MB
-
-type Thumbnail = {
-  data: ArrayBuffer;
-  mediaType: string;
-};
-
-const videoThumbnails: Map<string, Thumbnail> = new Map();
-
-export async function handlerGetThumbnail(cfg: ApiConfig, req: BunRequest) {
-  const { videoId } = req.params as { videoId?: string };
-  if (!videoId) {
-    throw new BadRequestError("Invalid video ID");
-  }
-
-  const video = getVideo(cfg.db, videoId);
-  if (!video) {
-    throw new NotFoundError("Couldn't find video");
-  }
-
-  const thumbnail = videoThumbnails.get(videoId);
-  if (!thumbnail) {
-    throw new NotFoundError("Thumbnail not found");
-  }
-
-  return new Response(thumbnail.data, {
-    headers: {
-      "Content-Type": thumbnail.mediaType,
-      "Cache-Control": "no-store",
-    },
-  });
-}
 
 export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
   const { videoId } = req.params as { videoId?: string };
@@ -54,6 +24,12 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
   if (!(thumbnailData instanceof File)) {
     throw new BadRequestError("Thumbnail form data not found on request");
   }
+  if (
+    thumbnailData.type !== "image/jpeg" &&
+    thumbnailData.type !== "image/png"
+  ) {
+    throw new BadRequestError("Only jpeg or png accepted. ");
+  }
   if (thumbnailData.size > MAX_UPLOAD_SIZE) {
     throw new BadRequestError("Thumbnail file size was over 10 MB");
   }
@@ -66,14 +42,13 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
     throw new UserForbiddenError("Video does not match the authenticated user");
   }
 
-  const thumbnail: Thumbnail = {
-    data: await thumbnailData.arrayBuffer(),
-    mediaType: thumbnailData.type,
-  };
-  videoThumbnails.set(videoId, thumbnail);
+  const arrayBuffer = await thumbnailData.arrayBuffer();
+  const ext = thumbnailData.type.split("/")[1];
+  const filePath = path.join(cfg.assetsRoot, `${video.id}.${ext}`);
 
-  video.thumbnailURL = `http://localhost:${cfg.port}/api/thumbnails/${videoId}`;
+  await Bun.write(filePath, arrayBuffer);
+
+  video.thumbnailURL = `http://localhost:${cfg.port}/assets/${video.id}.${ext}`;
   updateVideo(cfg.db, video);
-
   return respondWithJSON(200, video);
 }
